9f4f820aa8c8ee2b9f758bbf69ea00ebf91fe0dd
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-06-19 14:58:20 -0400

    redirect unauthorized

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'antd/dist/antd.css';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "antd/dist/antd.css";
 
 //components
-import NavBar from './components/Navbar/Navbar';
-import DogForm from './components/DogForm';
-import UserForm from './components/UserForm';
+import NavBar from "./components/Navbar/Navbar";
+import DogForm from "./components/DogForm";
+import UserForm from "./components/UserForm";
 
 //pages
-import Login from './pages/LogIn';
-import HomePage from './pages/HomePage';
-import UserProfile from './pages/UserProfile';
-import DogProfile from './pages/DogProfile';
-import Signup from './pages/Signup';
-import AddDog from './pages/AddDog';
-import CreateUser from './pages/CreateUser';
-
-
-
+import Login from "./pages/LogIn";
+import HomePage from "./pages/HomePage";
+import UserProfile from "./pages/UserProfile";
+import DogProfile from "./pages/DogProfile";
+import Signup from "./pages/Signup";
+import AddDog from "./pages/AddDog";
+import CreateUser from "./pages/CreateUser";
 
 //providers
-import { UserProvider } from './contexts/UserContext';
-import ProtectedRoute from './routeGuard/ProtectedRoute';
+import { UserProvider } from "./contexts/UserContext";
+import ProtectedRoute from "./routeGuard/ProtectedRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* Wrapping our entire App here, so that we can use context values anywhere (components, pages) */}
       <UserProvider>
-          <div className="App">
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="user/:id" element={<UserProfile />} />
-                <Route path="dog/:id" element={<DogProfile />} />
-               
-              </Route>
-              <Route path="login" element={<Login />} />
-              <Route path="signup" element={<Signup />} />
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="user/:id" element={<UserProfile />} />
+              <Route path="dog/:id" element={<DogProfile />} />
               <Route path="adddog" element={<AddDog />} />
               <Route path="createuser" element={<CreateUser />} />
-            </Routes>
-          </div>
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Routes>
+        </div>
       </UserProvider>
     </BrowserRouter>
   );
--- src/customAxios/authAxios.js
+++ src/customAxios/authAxios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 export const authAxios = axios.create({});
 
 //This function intercept the axios request and add "Authorization" header with the token.
@@ -11,4 +10,14 @@ authAxios.interceptors.request.use((config) => {
   return config;
 });
 
+authAxios.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    if (error.response.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+});
 //If any axios request needs authorization, you need to use authAxios instead of regular axios
