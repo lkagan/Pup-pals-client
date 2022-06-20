e0483ae9ddeb4fcd1df16f0f20588106b75bdc1e
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-06-20 16:28:17 -0400

    matches page working

--- src/pages/MyMatches.jsx
+++ src/pages/MyMatches.jsx
@@ -2,11 +2,29 @@
 // view profile of matched dog profile and their hooman
 // unmatch to delete dog from matches 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { authAxios } from '../customAxios/authAxios';
+
 
 function MyMatches() {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => { 
+    authAxios.get('http://localhost:5005/api/matches')
+      .then(response => {
+        setMatches(response.data)
+      })
+  }, []);
+
   return (
+    <>
     <div>MyMatches</div>
+    <ul>
+      { matches?.map(match => (
+        <li key={match._id}>{match.name}</li>
+      ))}
+    </ul>
+    </>
   )
 }
 
